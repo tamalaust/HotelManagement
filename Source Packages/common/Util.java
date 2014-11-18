ceaff8c96f490ad51815e56429965d3ae200bfca
Imran <Imran@Imran-PC>
2014-11-19 03:11:55 +0500

    Send registration Email & random password generation

--- Source Packages/common/Util.java
+++ Source Packages/common/Util.java
@@ -6,7 +6,7 @@
 package common;
 
 import java.util.Properties;
-import javax.mail.Authenticator;
+import java.util.UUID;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -21,39 +21,65 @@ import javax.mail.internet.MimeMessage;
 public class Util {
     
 
-    public static void SendEmail()
-    {Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
+    public static void SendEmail(String toEmail,String Name,String pwd)
+    { 
+        
+        
+        final String username = "waa.mum@gmail.com";
+        final String password = "mumwaa123";
+        StringBuilder messageBody  = new StringBuilder(); 
+       
+        
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
  
-		Session session = Session.getDefaultInstance(props,
-			new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("username","password");
-				}
-			});
+  Session session = Session.getInstance(props,
+    new javax.mail.Authenticator() {
+   protected PasswordAuthentication getPasswordAuthentication() {
+    return new PasswordAuthentication(username, password);
+   }
+    });
  
-		try {
+  try {
  
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("from@no-spam.com"));
-			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse("to@no-spam.com"));
-			message.setSubject("Testing Subject");
-			message.setText("Dear Mail Crawler," +
-					"\n\n No spam to my email, please!");
+   Message message = new MimeMessage(session);
+   message.setFrom(new InternetAddress("mum.asd2014@gmail.com"));
+   message.setRecipients(Message.RecipientType.TO,
+    InternetAddress.parse(toEmail));
+   message.setSubject("User Registration Confirmation");
+   
+ messageBody.append("Dear "+ Name+"\n\n");
+ messageBody.append("Welcome to the our hotel management system. Your credential are following\n\n");
+ messageBody.append("User Name: "+toEmail+" \n");
+ messageBody.append("Password: "+pwd+"\n\n");
+ messageBody.append("Please save this email these credintial are important for next login. Thanks for choosing our System\n\n");
+ messageBody.append("This is automated email please don't reply it\n\n");
+ messageBody.append("Best Regards\n");
  
-			Transport.send(message);
  
-			System.out.println("Done");
  
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
+   
+   message.setText(messageBody.toString());
+ 
+   Transport.send(message);
+ 
+   System.out.println("Done");
+ 
+  } catch (MessagingException e) {
+   throw new RuntimeException(e);
+  }
+ }
+
+    public static String generatePassword()
+    {
+    return UUID.randomUUID().toString().substring(0,7);
     }
-    
+
+
 }
+    
+    
+
