670e55e7ce24d22f79f95fd417660472067c1c53
zahid <mdzahid.islam@outlook.com>
2014-11-17 22:27:01 -0600

    (added rest of the entity  and bean skeleton ))

---
+++ Source Packages/mum/cs545/app/ReservationBean.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mum.cs545.app;
+
+/**
+ *
+ * @author zahid
+ */
+public class ReservationBean {
+    
+}
